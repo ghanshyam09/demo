f7857ccbbddff3b7d43498743007930c9c37ed97
ghanshyam09 <ghanshyampatil3157@example.com>
2019-10-02 18:03:31 +0530

    updated

--- app/src/main/java/com/example/demo/module/Questions.java
+++ app/src/main/java/com/example/demo/module/Questions.java
@@ -7,55 +7,55 @@ public class Questions
 
 
     public Questions(String question, String option1, String option2, String option3, String option4, String answer) {
-        this.question = question;
-        this.option1 = option1;
-        this.option2 = option2;
-        this.option3 = option3;
-        this.option4 = option4;
-        this.answer = answer;
+        this.question = question.toString();
+        this.option1 = option1.toString();
+        this.option2 = option2.toString();
+        this.option3 = option3.toString();
+        this.option4 = option4.toString();
+        this.answer = answer.toString();
     }
 
     public Questions()
     {}
 
     public String getQuestion() {
-        return question;
+        return question.toString();
     }
 
     public void setQuestion(String question) {
-        this.question = question;
+        this.question = question.toString();
     }
 
     public String getOption1() {
-        return option1;
+        return option1.toString();
     }
 
     public void setOption1(String option1) {
-        this.option1 = option1;
+        this.option1 = option1.toString();
     }
 
     public String getOption2() {
-        return option2;
+        return option2.toString();
     }
 
     public void setOption2(String option2) {
-        this.option2 = option2;
+        this.option2 = option2.toString();
     }
 
     public String getOption3() {
-        return option3;
+        return option3.toString();
     }
 
     public void setOption3(String option3) {
-        this.option3 = option3;
+        this.option3 = option3.toString();
     }
 
     public String getOption4() {
-        return option4;
+        return option4.toString();
     }
 
     public void setOption4(String option4) {
-        this.option4 = option4;
+        this.option4 = option4.toString();
     }
 
     public String getAnswer() {
@@ -63,6 +63,6 @@ public class Questions
     }
 
     public void setAnswer(String answer) {
-        this.answer = answer;
+        this.answer = answer.toString();
     }
 }
